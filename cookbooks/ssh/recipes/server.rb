32b2e6dedd82f107ebd72ec3d76385ec44417193
Aaron Bull Schaefer <aaron@elasticdog.com>
2010-09-26 19:55:47 -0700

    add initial ssh cookbook

---
+++ cookbooks/ssh/recipes/server.rb
@@ -0,0 +1,34 @@
+#
+# Cookbook Name:: ssh
+# Recipe:: default
+#
+# Copyright 2010, Aaron Bull Schaefer
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package "openssh" do
+  action :upgrade
+end
+
+users = search(:users)
+
+template "/etc/ssh/sshd_config" do
+  source "sshd_config.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  variables(
+    :users => search(:users)
+  )
+end
