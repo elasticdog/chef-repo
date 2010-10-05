38bfd4ad303878e31221f686e0480b696aa8b1e4
Aaron Bull Schaefer <aaron@elasticdog.com>
2010-10-04 18:53:40 -0700

    add initial fcgiwrap cookbook

---
+++ cookbooks/fcgiwrap/metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "Aaron Bull Schaefer"
+maintainer_email "aaron@elasticdog.com"
+license          "Apache 2.0"
+description      "Installs/Configures fcgiwrap"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+depends          "spawn-fcgi"
+version          "0.0.1"
---
+++ cookbooks/fcgiwrap/recipes/default.rb
@@ -0,0 +1,31 @@
+#
+# Cookbook Name:: fcgiwrap
+# Recipe:: default
+#
+# Copyright 2010, Aaron Bull Schaefer
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe "spawn-fcgi"
+
+package "fcgiwrap" do
+  :install
+end
+
+service "fcgiwrap" do
+  enabled true
+  running true
+  supports :restart => true
+  action [:enable, :start]
+end
